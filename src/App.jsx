b84f818c68f6edf202a298812da7304c2c66adab
pabitra bera <madeeasycodinglife@gmail.com>
2025-02-11 10:55:59 +0530

    home section added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <h1>Hi</h1>
+      <Home />
       <Footer />
     </div>
   );
